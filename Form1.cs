f19c39ba88c2ca9c7f7be7746fe3c8df8cab0003
Nikita <81682288+NaViMaTRiX@users.noreply.github.com>
2022-09-18 21:31:37 +0300

    all
    all

---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        int n, m, i, j;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rowNumber = dataGridView1.Rows.Add();
+            dataGridView1.Rows[rowNumber].Cells["ID"].Value = rowNumber;
+            dataGridView1.Rows[rowNumber].Cells[1].Value = textBox3.Text;
+            dataGridView1.Rows[rowNumber].Cells[2].Value = textBox2.Text;
+            dataGridView1.Rows[rowNumber].Cells["Tel"].Value = textBox1.Text;
+            dataGridView1.Rows[rowNumber].Cells["Email"].Value = textBox5.Text;
+            dataGridView1.Rows[rowNumber].Cells["Adr"].Value = textBox4.Text;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamReader f = new StreamReader("data.txt");
+            n = Convert.ToInt32(f.ReadLine());
+            m = Convert.ToInt32(f.ReadLine());
+            for (i = 0; i < n - 1; i++)
+            {
+                int rowNumber = dataGridView1.Rows.Add();
+                dataGridView1.Rows[rowNumber].Cells["ID"].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells[1].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells[2].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells["Tel"].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells["Email"].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells["Adr"].Value = f.ReadLine();
+
+
+            }
+            f.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            n = dataGridView1.RowCount;
+            m = dataGridView1.ColumnCount;
+            StreamWriter sw = File.CreateText("data.txt");
+            sw.WriteLine(Convert.ToString(n));
+            sw.WriteLine(Convert.ToString(m));
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    sw.WriteLine(Convert.ToString(dataGridView1[j, i].Value));
+                }
+            }
+            sw.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        
+                dataGridView1.Columns.Add("ID", "Id");
+                dataGridView1.Columns.Add("Fa", "Фамилия");
+                dataGridView1.Columns.Add("Name", "Имя");
+                dataGridView1.Columns.Add("Tel", "Телефон");
+                dataGridView1.Columns.Add("Email", "Эл.почта");
+                dataGridView1.Columns.Add("Adr", "Адрес");
+ 
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
